45adc6b1b207f67b75b70b01632914e47d2322ed
Thijs Brobbel <thijsbrobbel@gmail.com>
2025-03-23 23:37:20 +0100

    Get full format() text, and position

--- cmd/formatUsageCheck.go
+++ cmd/formatUsageCheck.go
@@ -1,15 +1,19 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 type formatUsages map[string][]formatInvocation
 
 type formatInvocation struct {
-	token hclsyntax.Token
+	expr   hcl.Expression
+	tokens []hclsyntax.Token
 }
 
 func checkForFormatUsage(files []string) (formatUsages, error) {
@@ -33,14 +37,49 @@ func checkForFormatUsageInFile(file string) ([]formatInvocation, error) {
 		return nil, err
 	}
 
-	var violations []formatInvocation
+	var violationTokens []hclsyntax.Token
 	for _, token := range tokens {
 		if isFormatToken(token) {
-			violations = append(violations, formatInvocation{token}) // token.Range.Start.Line
+			violationTokens = append(violationTokens, token)
 		}
 	}
 
-	return violations, nil
+	result, err := convertTokensToExpressions(file, tokens, violationTokens)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func convertTokensToExpressions(filename string, tokens []hclsyntax.Token, violationTokens []hclsyntax.Token) ([]formatInvocation, error) {
+	hclFile, diags := hclparse.NewParser().ParseHCLFile(filename)
+	if diags.HasErrors() {
+		return nil, errors.New("failed to parse TF file: " + diags.Error())
+	}
+
+	var result []formatInvocation
+	for _, t := range violationTokens {
+		expr := hclFile.OutermostExprAtPos(t.Range.Start)
+
+		result = append(result, formatInvocation{expr, getAllTokensForExpression(tokens, expr)})
+	}
+	return result, nil
+}
+
+func getAllTokensForExpression(tokens []hclsyntax.Token, expr hcl.Expression) []hclsyntax.Token {
+	start := 0
+	for i, t := range tokens {
+		if t.Range.Start == expr.Range().Start {
+			start = i
+		}
+
+		if t.Range.End == expr.Range().End {
+			return tokens[start:i+1]
+		}
+	}
+
+	panic("cannot reach")
 }
 
 func isFormatToken(token hclsyntax.Token) bool {
@@ -48,9 +87,14 @@ func isFormatToken(token hclsyntax.Token) bool {
 }
 
 func (invoke formatInvocation) string() string {
-	return string(invoke.token.Bytes)
+	var str string
+	for _, s := range invoke.tokens {
+		str = str + string(s.Bytes)
+	}
+	return str
 }
 
 func (invoke formatInvocation) location() string {
-	return fmt.Sprintf("%v:%d", invoke.token.Range.Filename, invoke.token.Range.Start.Line)
+	r := invoke.expr.Range()
+	return fmt.Sprintf("%v:L%d:%d-%d", r.Filename, r.Start.Line, r.Start.Column, r.End.Column)
 }
